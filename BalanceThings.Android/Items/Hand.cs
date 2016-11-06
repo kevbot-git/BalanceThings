e5e06a0e9e2fa44cd299c65996725021df41f8fe
kevbot-git <kamenhinick@gmail.com>
2016-11-06 23:45:03 +1300

    Changed hand collider to capsule

--- BalanceThings.Android/Items/Hand.cs
+++ BalanceThings.Android/Items/Hand.cs
@@ -12,8 +12,8 @@ namespace BalanceThings.Items
     class Hand : GameObject
     {
         internal Hand(World world, ContentManager contentManager, Vector2 position)
-            : base(BodyFactory.CreateEllipse(world, ConvertUnits.ToSimUnits(1f), ConvertUnits.ToSimUnits(3f), 8, 1f,
-                ConvertUnits.ToSimUnits(position + new Vector2(6, 1))),
+            : base(BodyFactory.CreateCapsule(world, ConvertUnits.ToSimUnits(6f), ConvertUnits.ToSimUnits(1f), 2,
+                ConvertUnits.ToSimUnits(1f), 2, 1f, ConvertUnits.ToSimUnits(position + new Vector2(6, 1))),
                   new Sprite(contentManager.Load<Texture2D>("img/hand_default"), new Rectangle(6, 1, 2, 6), position, 1f, 0f))
         {
             Body.BodyType = BodyType.Static;
