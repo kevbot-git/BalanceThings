254f059f0065792f651d74d89cabecfaa736a0cb
kevbot-git <kamenhinick@gmail.com>
2016-11-06 23:39:23 +1300

    Changed hand collider to ellipse

--- BalanceThings.Android/Items/Hand.cs
+++ BalanceThings.Android/Items/Hand.cs
@@ -1,6 +1,8 @@
 using BalanceThings.Drawing;
 using BalanceThings.Physics;
+using FarseerPhysics;
 using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -10,7 +12,9 @@ namespace BalanceThings.Items
     class Hand : GameObject
     {
         internal Hand(World world, ContentManager contentManager, Vector2 position)
-            : base(world, new Sprite(contentManager.Load<Texture2D>("img/hand_default"), new Rectangle(6, 1, 2, 6), position, 1f, 0f), 1f)
+            : base(BodyFactory.CreateEllipse(world, ConvertUnits.ToSimUnits(1f), ConvertUnits.ToSimUnits(3f), 8, 1f,
+                ConvertUnits.ToSimUnits(position + new Vector2(6, 1))),
+                  new Sprite(contentManager.Load<Texture2D>("img/hand_default"), new Rectangle(6, 1, 2, 6), position, 1f, 0f))
         {
             Body.BodyType = BodyType.Static;
             Body.IsKinematic = true;
